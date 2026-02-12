84bd9a28074f05ef6e8a1804f20c5e912da4b998
Ahmed <ahmedgacuni@gmail.com>
2026-02-12 15:59:04 +0300

    Chapter 4: Added templates and index.html

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hello world")
+    return render(request, 'rango/index.html')
